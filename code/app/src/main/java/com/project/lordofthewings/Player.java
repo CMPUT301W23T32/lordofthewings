c838f280067135f1cd2b12f9c5d2e4f9e636d15a
Jianghui-Lai <laijianghui@gmail.com>
2023-02-28 16:35:59 -0700

    add a Layer class

---
+++ code/app/src/main/java/com/project/lordofthewings/Player.java
@@ -0,0 +1,49 @@
+package com.project.lordofthewings;
+//import QRCode
+//import wallet
+import java.util.ArrayList;
+//friends is a list of players
+import java.util.Map;
+
+public class Player {
+    private String userName;
+    private String id;
+    //private String Name;
+
+    //private int score;
+
+    //private String email;
+    //private ArrayList<QRCode>  QRList;
+    private Wallet QRWallet;
+    //private Leaderboard leaderboard;
+    //public QRcode scan(){
+        //return  QRcode;
+    //};
+
+//    public Map dis playMap(){
+//
+//    };
+
+//    public Player SearchPlayer(){
+//        return player;
+//    }
+
+    public  String getName(){
+        return this.userName;
+    }
+
+    public void setName (String Name){
+        this.userName = Name;
+    }
+
+
+    public String getUserName(String Name){}
+
+//   public scan(Camera camera){
+//        //add to
+//   }
+
+
+
+
+}
