4cab930ec21c551d4b146dbc1fb219e004dc0465
Krish <krishlal4204@gmail.com>
2023-03-05 16:40:48 -0700

    added the basic homepage

--- code/app/src/main/java/com/project/lordofthewings/Views/HomePage.java
+++ code/app/src/main/java/com/project/lordofthewings/Views/HomePage.java
@@ -9,13 +9,16 @@ import androidx.appcompat.app.AppCompatActivity;
 import com.project.lordofthewings.R;
 
 public class HomePage extends AppCompatActivity {
+    TextView usernametext;
     @Override
     protected void onCreate(Bundle savedInstance){
         super.onCreate(savedInstance);
         setContentView(R.layout.home_page);
-        TextView hometext = findViewById(R.id.TextView01);
+        //TextView hometext = findViewById(R.id.TextView01);
         SharedPreferences sh = getSharedPreferences("sharedPrefs", MODE_PRIVATE);
         String username = sh.getString("username", "");
-        hometext.setText("Welcome " + username + "!");
+        //hometext.setText("Welcome " + username + "!");
+        usernametext = findViewById(R.id.usernameTextView);
+        usernametext.setText(username);
     }
 }
