e2858119e9a6f2357ef02b0a41b7ca999b96081f
mansooranis <mansooranis60@gmail.com>
2023-03-05 17:03:46 -0700

    added the functionality of adding a qrcode

---
+++ code/app/src/main/java/com/project/lordofthewings/Views/CameraPages/QRCodeScan.java
@@ -0,0 +1,140 @@
+package com.project.lordofthewings.Views.CameraPages;
+
+import android.Manifest;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+import com.project.lordofthewings.R;
+
+public class QRCodeScan extends AppCompatActivity implements LocationListener {
+    private static final int CAMERA_REQUEST = 1888;
+    ImageView imageView;
+    Button add_photo;
+    Button remove_photo;
+    Button save_location;
+    Button remove_location;
+    TextView location_text;
+    String qr_code;
+    protected LocationManager locationManager;
+    protected LocationListener locationListener;
+    protected Context context;
+    String lat;
+    String provider;
+    protected String latitude, longitude;
+    protected boolean gps_enabled, network_enabled;
+
+    @Override
+    protected void onCreate(Bundle savedInstance) {
+        super.onCreate(savedInstance);
+        setContentView(R.layout.qr_code_scan);
+        Bundle b = getIntent().getExtras();
+        qr_code = b.getString("qr_code");
+        TextView code = findViewById(R.id.qr_code_text);
+        code.setText(qr_code);
+        add_photo = findViewById(R.id.add_image_button);
+        imageView = findViewById(R.id.location_image);
+        location_text = findViewById(R.id.location_added);
+        remove_photo = findViewById(R.id.remove_image_button);
+        remove_location = findViewById(R.id.remove_location_button);
+        location_text.setText("Location Not Added");
+        location_text.setVisibility(TextView.VISIBLE);
+        remove_location.setOnClickListener(c -> {
+            location_text.setText("Location Not Added");
+            latitude = "";
+            longitude = "";
+            remove_location.setVisibility(Button.GONE);
+            save_location.setVisibility(Button.VISIBLE);
+        });
+        add_photo.setOnClickListener(c -> {
+            Intent cameraIntent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+            startActivityForResult(cameraIntent, CAMERA_REQUEST);
+        });
+        remove_photo.setOnClickListener(c -> {
+            imageView.setVisibility(ImageView.GONE);
+            add_photo.setVisibility(Button.VISIBLE);
+            remove_photo.setVisibility(Button.GONE);
+        });
+        save_location = findViewById(R.id.add_location_button);
+        save_location.setOnClickListener(c -> {
+            locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                locationPermissionRequest.launch(new String[]{
+                        Manifest.permission.ACCESS_FINE_LOCATION,
+                        Manifest.permission.ACCESS_COARSE_LOCATION
+                });
+                return;
+            }
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, this);
+            Location location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+            latitude = String.valueOf(location.getLatitude());
+            longitude = String.valueOf(location.getLongitude());
+            location_text.setText("Location Added: " + latitude + ", " + longitude);
+            location_text.setVisibility(TextView.VISIBLE);
+            save_location.setVisibility(Button.GONE);
+            remove_location.setVisibility(Button.VISIBLE);
+        });
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == CAMERA_REQUEST) {
+            Bitmap photo = (Bitmap) data.getExtras().get("data");
+            imageView.setImageBitmap(photo);
+            imageView.setVisibility(ImageView.VISIBLE);
+            add_photo.setVisibility(Button.GONE);
+            remove_photo.setVisibility(Button.VISIBLE);
+        }
+    }
+
+    ActivityResultLauncher<String[]> locationPermissionRequest =
+            registerForActivityResult(new ActivityResultContracts
+                            .RequestMultiplePermissions(), result -> {
+                        Boolean fineLocationGranted = result.getOrDefault(
+                                Manifest.permission.ACCESS_FINE_LOCATION, false);
+                        Boolean coarseLocationGranted = result.getOrDefault(
+                                Manifest.permission.ACCESS_COARSE_LOCATION, false);
+                        if (fineLocationGranted != null && fineLocationGranted) {
+                            // Precise location access granted.
+                        } else if (coarseLocationGranted != null && coarseLocationGranted) {
+                            // Only approximate location access granted.
+                        } else {
+                            // No location access granted.
+                        }
+                    }
+            );
+
+    public void onLocationChanged(Location location) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+        Log.d("Latitude", "disable");
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+        Log.d("Latitude", "enable");
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+        Log.d("Latitude", "status");
+    }
+}
--- code/app/src/main/java/com/project/lordofthewings/Views/HomePage.java
+++ code/app/src/main/java/com/project/lordofthewings/Views/HomePage.java
@@ -1,12 +1,20 @@
 package com.project.lordofthewings.Views;
 
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
 import com.project.lordofthewings.R;
+import com.project.lordofthewings.Views.CameraPages.QRCodeScan;
 
 public class HomePage extends AppCompatActivity {
     @Override
@@ -17,5 +25,31 @@ public class HomePage extends AppCompatActivity {
         SharedPreferences sh = getSharedPreferences("sharedPrefs", MODE_PRIVATE);
         String username = sh.getString("username", "");
         hometext.setText("Welcome " + username + "!");
+        Button scan_qr_code = findViewById(R.id.camera_button);
+        scan_qr_code.setOnClickListener(c -> {
+            IntentIntegrator integrator = new IntentIntegrator(this);
+            integrator.setPrompt("Scan a QR Code");
+            integrator.setOrientationLocked(false);
+            integrator.initiateScan();
+        });
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
+        if (result != null) {
+            if (result.getContents() == null) {
+                Log.d("MainActivity", "Cancelled scan");
+                Toast.makeText(this, "Cancelled", Toast.LENGTH_LONG).show();
+            } else {
+                Log.d("MainActivity", "Scanned");
+                String qr_code = result.getContents();
+                Intent intent = new Intent(HomePage.this, QRCodeScan.class);
+                intent.putExtra("qr_code", qr_code);
+                startActivity(intent);
+            }
+        } else {
+            super.onActivityResult(requestCode, resultCode, data);
+        }
     }
 }
