898d6b7ae52c3c52cf481462d6a6ec211cfa0029
Krish <krishlal4204@gmail.com>
2023-03-05 17:49:17 -0700

    removed the button image, will fix that later

--- code/app/src/main/java/com/project/lordofthewings/Views/HomePage.java
+++ code/app/src/main/java/com/project/lordofthewings/Views/HomePage.java
@@ -1,5 +1,6 @@
 package com.project.lordofthewings.Views;
 
+import android.annotation.SuppressLint;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.widget.TextView;
@@ -10,6 +11,7 @@ import com.project.lordofthewings.R;
 
 public class HomePage extends AppCompatActivity {
     TextView usernametext;
+    @SuppressLint("SetTextI18n")
     @Override
     protected void onCreate(Bundle savedInstance){
         super.onCreate(savedInstance);
@@ -19,6 +21,6 @@ public class HomePage extends AppCompatActivity {
         String username = sh.getString("username", "");
         //hometext.setText("Welcome " + username + "!");
         usernametext = findViewById(R.id.usernameTextView);
-        usernametext.setText(username);
+        usernametext.setText("Welcome " +  username + "!");
     }
 }
