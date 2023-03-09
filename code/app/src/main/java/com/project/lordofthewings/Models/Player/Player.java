64bedd0c46a479d56032ef7eab0c52be0edd7d3b
Krish <krishlal4204@gmail.com>
2023-03-08 23:59:29 -0700

    new player class constructors and db fetch

--- code/app/src/main/java/com/project/lordofthewings/Models/Player/Player.java
+++ code/app/src/main/java/com/project/lordofthewings/Models/Player/Player.java
@@ -12,8 +12,11 @@ import com.google.firebase.firestore.Query;
 import com.google.firebase.firestore.QuerySnapshot;
 import com.google.firebase.firestore.core.FirestoreClient;
 import com.project.lordofthewings.Controllers.FirebaseController;
+import com.project.lordofthewings.Models.QRcode.QRCode;
+import com.project.lordofthewings.Models.Wallet.Wallet;
 
 //friends is a list of players
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.concurrent.CompletableFuture;
@@ -24,33 +27,106 @@ public class Player {
     private String lastName;
     private String email;
 
-    //private ArrayList<QRCode>  QRList;
-    //private Wallet QRWallet;
+
+    private int score;
+    private ArrayList<QRCode>  QRList;
+
+    private Wallet QRWallet;
     //private Leaderboard leaderboard;
     FirebaseController fbcontroller = new FirebaseController();
     FirebaseFirestore db = fbcontroller.getDb();
 
-    public Player(String userName){
+//    public Player(String userName){
+//        this.userName = userName;
+//        db.collection("Users").document(userName).get().addOnCompleteListener(task -> {
+//            if (task.isSuccessful()) {
+//                Map<String, Object> data = task.getResult().getData();
+//                assert data != null;
+//                this.email = (String) data.get("email");
+//            }
+//        });
+//    }
+
+
+    //use this constructor to instantiate a new player during signup
+    public Player(String userName, String email, String firstName, String lastName, FirebaseFirestore db) throws Exception{
+        if(checkIfUserExists(userName,db).get()){
+            throw new Exception("Username already exists!");
+        }
+        this.score = 0;
+        this.QRList = new ArrayList<>();
+        this.userName = userName;
+        this.email = email;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+
+    //use this constructor to instantiate a player that already exists in the database
+    public Player(String userName, FirebaseFirestore db) throws Exception{
+        CompletableFuture<Boolean> future = checkIfUserExists(userName, db);
+        Boolean exists = future.get();
+
+        if(!exists){
+            throw new Exception("Username does not exist!");
+        }
         this.userName = userName;
         db.collection("Users").document(userName).get().addOnCompleteListener(task -> {
             if (task.isSuccessful()) {
                 Map<String, Object> data = task.getResult().getData();
                 assert data != null;
                 this.email = (String) data.get("email");
+                this.firstName = (String) data.get("firstName");
+                this.lastName = (String) data.get("lastName");
+                this.score = (int) data.get("score");
             }
         });
     }
 
-//    public Player(String userName, String email, String firstName, String lastName){
-//        this.userName = userName;
-//        this.email = email;
-//        this.firstName = firstName;
-//        this.lastName = lastName;
-//    }
+
+
+    public CompletableFuture<Boolean> checkIfUserExists(String userName, FirebaseFirestore db){
+        CollectionReference playerRef = db.collection("Users");
+        Query query = playerRef.whereEqualTo("username",userName);
+        CompletableFuture<Boolean> future = new CompletableFuture<>();
+        query.get().addOnSuccessListener(querySnapshot -> {
+            boolean exists =  querySnapshot.size() > 0;
+            future.complete(exists);
+        }).addOnFailureListener(error -> {
+            future.completeExceptionally(error);
+        });
+        return future;
+    }
 
 
 
+    /*
+    2 constructors,
 
+    function: create a new user
+
+    mainly for signup
+
+    1st constructor only takes a username, checks if it exists
+    if it exists then it tells the user that username is reserved
+    and you gotta choose another username otherwise if it doesn't exist, add the player
+
+    to just retrieve the values
+
+     2nd constructor-:
+     now the username should exist, no matter what and then retrieve the values
+
+     if we want a username
+     */
+
+
+    public int getScore(){
+        return this.score;
+    }
+
+    public int getNumOfQRCode(){
+        return this.QRList.size();
+    }
 
     public void setEmail(String email){
         this.email = email;
--- code/app/src/main/java/com/project/lordofthewings/Views/StartUpPages/SignUpPage.java
+++ code/app/src/main/java/com/project/lordofthewings/Views/StartUpPages/SignUpPage.java
@@ -4,6 +4,9 @@ import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.os.Message;
 import android.util.Log;
 import android.widget.Button;
 import android.widget.EditText;
@@ -19,11 +22,13 @@ import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.Query;
 import com.project.lordofthewings.Controllers.FirebaseController;
 import com.project.lordofthewings.Models.Player.Player;
+import com.project.lordofthewings.Models.QRcode.QRCode;
 import com.project.lordofthewings.R;
 import com.project.lordofthewings.Views.HomePage;
 
 import org.apache.commons.codec.digest.DigestUtils;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.concurrent.CompletableFuture;
@@ -45,61 +50,66 @@ public class SignUpPage extends AppCompatActivity {
             String emailRes = email.getText().toString();
             String firstNameRes = firstName.getText().toString();
             String lastNameRes = lastName.getText().toString();
-            Log.d("Breakpoint","Reached here42");
+
+
+
+
+
+
+
             if (!usernameRes.equals("") && !firstNameRes.equals("") && !lastNameRes.equals("") && !emailRes.equals("")) {
                 FirebaseController fbcontroller = new FirebaseController();
 //                FirebaseFirestore db = FirebaseFirestore.getInstance();
                 FirebaseFirestore db = fbcontroller.getDb();
-                Log.d("Breakpoint","Reached here");
-                //check if user exists
-                CompletableFuture<Boolean> future = checkIfUserExists(usernameRes, db);
-                future.thenApply(exists -> {
-                    if (exists) {
-                        //write a toast here that user already exists
-                        Toast.makeText(SignUpPage.this, "Username already exists!", Toast.LENGTH_SHORT).show();
-                        Log.d("TEST", "User already exists");
+
+                Log.d("Breakpoint", "Reached here");
+                CompletableFuture<Player> future = CompletableFuture.supplyAsync(() -> {
+                    try {
+                        return new Player(usernameRes, emailRes, firstNameRes, lastNameRes, db);
+                    } catch (Exception e) {
+                        runOnUiThread(() -> Toast.makeText(SignUpPage.this, "Username already exists!", Toast.LENGTH_SHORT).show());
+                        throw new RuntimeException(e);
                     }
-                    else{
-                        Log.d("Breakpoint","Reached here2");
+                });
+                future.thenApply(player -> {
+                        Log.d("Breakpoint", "Reached here2");
                         Map<String, Object> user = addPlayertoDB(usernameRes, emailRes, firstNameRes, lastNameRes);
                         db.collection("Users").document(usernameRes).set(user)
-                                .addOnSuccessListener(new OnSuccessListener<Void>() {
-                                    @Override
-                                    public void onSuccess(Void aVoid) {
-                                        Log.d("Success in writing", "DocumentSnapshot successfully written!");
-                                        Intent intent = new Intent(SignUpPage.this, HomePage.class);
-                                        SharedPreferences sh = getSharedPreferences("sharedPrefs", MODE_PRIVATE);
-                                        SharedPreferences.Editor editor = sh.edit();
-                                        editor.putString("username", usernameRes);
-                                        editor.apply();
-                                        startActivity(intent);
-                                    }
+                                .addOnSuccessListener(aVoid -> {
+                                    Log.d("Success in writing", "DocumentSnapshot successfully written!");
+                                    Intent intent = new Intent(SignUpPage.this, HomePage.class);
+                                    SharedPreferences sh = getSharedPreferences("sharedPrefs", MODE_PRIVATE);
+                                    SharedPreferences.Editor editor = sh.edit();
+                                    editor.putString("username", usernameRes);
+                                    editor.apply();
+                                    startActivity(intent);
                                 })
-                                .addOnFailureListener(new OnFailureListener() {
-                                    @Override
-                                    public void onFailure(@NonNull Exception e) {
-                                        Log.e("Failure in writing", "Error writing document", e);
-                                    }
+                                .addOnFailureListener(e -> {
+                                    Log.e("Failure in writing", "Error writing document", e);
                                 });
-
-                    }
+                    return null;
+                }).exceptionally(e -> {
+                    Log.e("Error in creating player","Error" , e);
+                    runOnUiThread(() -> Toast.makeText(SignUpPage.this, "Username already exists!", Toast.LENGTH_SHORT).show());
                     return null;
                 });
+            } else {
+                Toast.makeText(SignUpPage.this, "Please fill in all the fields!", Toast.LENGTH_SHORT).show();
+            }
 
-        }
-        else{
-            Toast.makeText(SignUpPage.this, "Please fill in all the fields!", Toast.LENGTH_SHORT).show();
-        }
-    });
-}
+        });
+    }
+
 
 
 
     //method to check if the user exists asynchronously
+
+
+
     public CompletableFuture<Boolean> checkIfUserExists(String userName, FirebaseFirestore db){
         CollectionReference playerRef = db.collection("Users");
         Query query = playerRef.whereEqualTo("username",userName);
-
         CompletableFuture<Boolean> future = new CompletableFuture<>();
         query.get().addOnSuccessListener(querySnapshot -> {
             boolean exists =  querySnapshot.size() > 0;
@@ -110,6 +120,7 @@ public class SignUpPage extends AppCompatActivity {
 
         return future;
     }
+
 
     public Map<String, Object> addPlayertoDB(String userName, String email,String firstName, String lastName){
         //if you want to use the player object
@@ -122,10 +133,13 @@ public class SignUpPage extends AppCompatActivity {
 //        player.setEmail(email);
         //if you want to just put values to the db
         Map<String, Object> user = new HashMap<>();
+        ArrayList<QRCode> qrcodes = new ArrayList<>();
         user.put("username", userName);
         user.put("email", email);
         user.put("firstName", firstName);
         user.put("lastName", lastName);
+        user.put("QRCodes", qrcodes);
+        user.put("Score",0);
         return user;
     }
 }
