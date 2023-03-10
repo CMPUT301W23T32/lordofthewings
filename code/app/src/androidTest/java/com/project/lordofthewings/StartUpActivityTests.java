d26eb7722e192731f0f38c2c2613ab0e451eb276
“Moeid <moeidmk@gmail.com>
2023-03-10 12:47:02 -0700

    finished the framerwork for intent testing now just need to add tests

---
+++ code/app/src/androidTest/java/com/project/lordofthewings/StartUpActivityTests.java
@@ -0,0 +1,74 @@
+package com.project.lordofthewings;
+
+import android.app.Activity;
+
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.platform.app.InstrumentationRegistry;
+import androidx.test.rule.ActivityTestRule;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import com.project.lordofthewings.Views.MainActivity;
+import com.project.lordofthewings.Views.StartUpPages.StartUpPage;
+import com.robotium.solo.Solo;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static junit.framework.TestCase.assertTrue;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+
+/**
+ * Test class for MainActivity. All the UI tests are written here. Robotium test framework is used
+ */
+@RunWith(AndroidJUnit4.class)
+public class StartUpActivityTests {
+    private Solo solo;
+
+    @Rule
+    public ActivityTestRule<StartUpPage> rule =
+            new ActivityTestRule<>(StartUpPage.class, true, true);
+    /**
+     * Runs before all tests and creates solo instance.
+     * @throws Exception
+     */
+    @Before
+    public void setUp() throws Exception{
+
+        solo = new Solo(InstrumentationRegistry.getInstrumentation(),rule.getActivity());
+    }
+
+    /**
+     * Gets the Activity
+     * @throws Exception
+     */
+    @Test
+    public void start() throws Exception{
+        Activity activity = rule.getActivity();
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+    /**
+     * Close activity after each test
+     * @throws Exception
+     */
+    @After
+    public void tearDown() throws Exception{
+        solo.finishOpenedActivities();
+    }
+
+}
